114998e441fca55e4b78966fd0ea46956ab374f2
Gustavo Ferreira <gustavodelferreira.12@gmail.com>
2022-11-03 09:19:31 -0300

    changed app name

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,22 +8,22 @@ interface ILayoutProps {
 const Layout: React.FC<ILayoutProps> = ({ children }) => {
   return (
     <>
-      <div className="bg-teal-700">
+      <div className="bg-teal-700 shadow-md fixed top-0 w-screen">
         <header className="container mx-auto flex justify-between items-center py-3 text-white font-semibold">
           <h1 className="text-2xl cursor-pointer hover:text-teal-100">
-            Dummy Meetups
+            RandoMemes
           </h1>
           <nav className="flex gap-3">
             <Link href="/">
-              <a className="hover:text-teal-100">All Meetups</a>
+              <a className="hover:text-teal-100">All Memes</a>
             </Link>
             <Link href="/new-meetup">
-              <a className="hover:text-teal-100">Add New Meetup</a>
+              <a className="hover:text-teal-100">Add New Meme</a>
             </Link>
           </nav>
         </header>
       </div>
-      <main className="container mx-auto">{children}</main>
+      <main className="container mx-auto py-14">{children}</main>
     </>
   )
 }
